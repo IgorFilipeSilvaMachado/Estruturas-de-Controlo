d80bc4c3bf0725ac567848d3d7ae04c897d1d9c4
IgorFilipeSilvaMachado <56729271+IgorFilipeSilvaMachado@users.noreply.github.com>
2020-06-13 15:28:56 +0100

    Add files via upload

---
+++ Estruturas de Controlo.java
@@ -0,0 +1,47 @@
+-----------If-----------
+
+if (x < 10){
+	Printf("Ganhou");
+}
+else{
+	Printf("Perdeu");
+}
+
+-----------Switch-----------
+
+switch (day){
+	case a:
+		Printf("Segunda-Feira");
+		break;
+
+	case b:
+		Printf("Terça-Feira");
+		break;
+
+	case c:
+		Printf("Quarta-Feira");
+		break;
+
+	case d:
+		Printf("Quinta-Feira");
+		break;
+
+	case e:
+		Printf("Sexta-Feira");
+		break;
+
+	case f:
+		Printf("Sábado");
+		break;
+
+	case g:
+		Printf("Domingo");
+		break;
+}
+
+-----------For-----------
+
+for (i = 0; i < 10; i++)
+{
+	num = num + i;
+}
